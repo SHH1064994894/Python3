a99fa854fe669bca0838b4cae0e08a5ccc6409ce
shh <1064994894@qq.com>
2016-09-30 10:02:21 +0800

    9.30 v0.1

--- Basics/ch03_function/01_call_function.py
+++ Basics/ch03_function/01_call_function.py
@@ -1,1 +1,22 @@
+#coding=utf-8 
+#调用内置函数
  
+#abs(int) 绝对值
+print(abs(100))
+#max(int...) 取最大值
+print(max(1,2,100,200,300,564))
+ 
+ 
+#数据类型转换
+print(int('123'))
+print(float('12.34'))
+print(str(123))
+print(bool(1))
+print(bool(''))
+
+#work
+#Python内置的hex()函数把一个整数转换成十六进制表示的字符串
+num1 = input("请输入第1个数:\n")
+num2 = input("请输入第2个数:\n")
+print("转换hex值为 第1个数hex=%s 第2个数hex=%s" % (hex(int(num1)),hex(int(num2))))
+#print("转换hex值为 第1个数hex=%x 第2个数hex=%x" % ((int(num1)),(int(num2))))
--- Basics/ch03_function/02_definition.py
+++ Basics/ch03_function/02_definition.py
@@ -1,1 +1,39 @@
- 
+#coding=utf-8  
+
+#定义函数
+def conver_hex(num1,num2):
+	#print("转换hex值为 第1个数hex=%s 第2个数hex=%s" % (hex(int(num1)),hex(int(num2))))
+	#return hex(int(num1)),hex(int(num2)
+	return num1,num2
+	
+#调用函数
+#conver_hex(1,2)
+
+#空函数
+def nop():
+	pass
+#参数检查 isinstance()
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-100))
+
+#函数返回多个值类型为tuple
+nums = conver_hex(1,2)
+print(conver_hex(1,2))
+
+#work 
+
+
+
+
+
+
+
+
+
+
